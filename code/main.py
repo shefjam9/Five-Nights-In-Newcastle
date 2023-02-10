80ea8e648dd803a6225f3fd1d4ac8df795775fc8
Julian <julianjones663@gmail.com>
2023-02-10 20:06:42 +0000

    Made screen white

--- code/main.py
+++ code/main.py
@@ -1,1 +1,30 @@
 import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Create a screen
+screen = pygame.display.set_mode((800, 600))
+
+def run_game():
+    # Game loop
+    running = True
+    while running:
+        try:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # Draw background
+            screen.fill((255, 255, 255))
+
+            # Update
+            pygame.display.update()
+        except KeyboardInterrupt:
+            running = False
+            
+    # End game
+    pygame.quit()
+
+if __name__ == "__main__":
+    run_game()
