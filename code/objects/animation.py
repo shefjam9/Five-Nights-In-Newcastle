3a62a50765e3a31f495995fb5bc9bb0355fa6f82
joant <joantpat@gmail.com>
2023-02-11 20:34:00 +0000

    Commented

--- code/objects/animation.py
+++ code/objects/animation.py
@@ -21,10 +21,12 @@ class Animation:
         self.updates_per_frame = upf
     
     def update(self, time):
+        """ Update animation so frames change """
         if self.current_updates >= self.updates_per_frame:
             self.current_frame = (self.current_frame+1)%self.num_frames
             self.current_updates -= self.updates_per_frame
         self.current_updates+=1
     
     def render_frame(self, surface: pygame.Surface, x: int, y: int) -> None:
+        """ render the current animation """
         surface.blit(self.tex, (x, y), (self.current_frame*self.width, 0, self.width, self.height))
