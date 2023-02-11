6143d467e47b32e4a49c2eb288b7596b1a12039f
Julian <julianjones663@gmail.com>
2023-02-11 18:49:49 +0000

    Fixed merge

--- code/loops/game_loop.py
+++ code/loops/game_loop.py
@@ -22,11 +22,6 @@ class GameLoop:
 
         # Add entities
         self.add_entity(self.player)
-<<<<<<< HEAD
-        for _ in range(10):
-            self.add_entity(Glass(time.perf_counter()*1e3, random.randint(100, 1200), random.randint(100, 1200), self.player))
-=======
->>>>>>> dadee088629231f391f17932b8794e8ed83560c6
 
     def add_entity(self, entity: pygame.sprite.Sprite):
         """Add an entity to the game (needs update method)"""
