1a66a903a17409b7cea4f622db7701d61cfa5694
joant <joantpat@gmail.com>
2023-02-12 04:52:37 +0000

    Added ESC exit

--- client-2/main.py
+++ client-2/main.py
@@ -57,7 +57,7 @@ def _handle_input():
   """ Handle all input"""
   global _running
   for event in pygame.event.get():
-    if event.type == pygame.QUIT:
+    if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
       _running = False
     else:
       ObstacleManager.handle_input(event)
